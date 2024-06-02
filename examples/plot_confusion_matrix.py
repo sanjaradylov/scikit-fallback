1be37975d86f70783b67a2fbf36f479680eefb6d
Sanjar Ad[yi]lov <16402077+sanjaradylov@users.noreply.github.com>
2024-06-02 19:14:39 +0500

    🏫 Use MLP and plot rejected random samples.

--- examples/plot_confusion_matrix.py
+++ examples/plot_confusion_matrix.py
@@ -1,16 +1,17 @@
 """
-=======================================================
-Logistic regression w/ and w/o a reject option on MNIST
-=======================================================
+=======================================
+MLP w/ and w/o a reject option on MNIST
+=======================================
 
 An example plot of :class:`skfb.metrics.PAConfusionMatrixDisplay`.
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.datasets import fetch_openml
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
@@ -18,6 +19,23 @@ from skfb.estimators import ThresholdFallbackClassifier
 from skfb.metrics import PAConfusionMatrixDisplay
 
 
+# pylint: disable=redefined-outer-name
+def generate_random_combination(X):
+    """Generates random combination of two images from different class."""
+    alpha = np.random.uniform(0.2, 0.8)
+    first = X[np.random.choice(X.shape[0])]
+    second = X[np.random.choice(X.shape[0])]
+    random = alpha * first + (1 - alpha) * second
+
+    add_noise = np.random.choice(2)
+    if add_noise:
+        random = np.random.randint(0, 256, size=random.shape) + random
+        random = np.clip(random, 0, 256)
+
+    return random
+
+
+# region Get dataset
 X, y = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False)
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -27,25 +45,57 @@ X_train, X_test, y_train, y_test = train_test_split(
     shuffle=True,
     stratify=y,
 )
+# endregion
 
-estimator = LogisticRegression(
-    C=0.01,
-    penalty="l1",
-    solver="saga",
-    tol=0.01,
+# region Train rejector and classifier
+estimator = MLPClassifier(
+    hidden_layer_sizes=(40,),
+    max_iter=20,
+    alpha=1e-2,
+    solver="adam",
     random_state=0,
+    learning_rate_init=0.005,
+    early_stopping=True,
+    validation_fraction=0.11,
+    n_iter_no_change=5,
+    tol=1e-3,
+    verbose=10,
 )
 pipe = make_pipeline(StandardScaler(), estimator)
-rejector = ThresholdFallbackClassifier(pipe, threshold=0.5, fallback_mode="return")
+rejector = ThresholdFallbackClassifier(pipe, threshold=0.8, fallback_mode="return")
 rejector.fit(X_train, y_train)
+# endregion
 
-print("LogisticRegression without fallbacks:")
+# region Evaluate
+print("MLPClassifier without fallbacks:")
 print(f"Test accuracy: {rejector.estimator_.score(X_train, y_train) * 100.0:.2f}%")
 print(f"Test accuracy: {rejector.estimator_.score(X_test, y_test) * 100.0:.2f}%")
 
-print("\nLogisticRegression with fallbacks:")
+print("\nMLPClassifier with fallbacks:")
 print(f"Test accuracy: {rejector.score(X_train, y_train) * 100.0:.2f}%")
 print(f"Test accuracy: {rejector.score(X_test, y_test) * 100.0:.2f}%")
 
 PAConfusionMatrixDisplay.from_estimator(rejector, X_test, y_test).plot()
+# endregion
+
+# region Generate random images and plot rejections
+X_random = np.array([generate_random_combination(X) for _ in range(14_000)])
+X_comb = np.concatenate([X_test, X_random])
+y_score = rejector.predict_proba(X_comb)
+fallback_mask = y_score.get_dense_fallback_mask()
+scale = X_comb.max(axis=None)
+plt.figure(figsize=(7, 7))
+for i in range(30):
+    plot = plt.subplot(5, 6, i + 1)
+    plt.imshow(
+        X_comb[fallback_mask][np.random.choice(fallback_mask.sum())].reshape(28, 28),
+        interpolation="nearest",
+        cmap=plt.cm.RdBu,
+        vmin=-scale,
+        vmax=scale,
+    )
+    plot.set_xticks(())
+    plot.set_yticks(())
+plt.suptitle("Examples of rejected digits")
 plt.show()
+# endregion
