0dfcd7f48b90c7f9c38ddd2fa80080a25944b8ae
Sanjar Ad[yi]lov <16402077+sanjaradylov@users.noreply.github.com>
2026-01-24 17:44:35 +0500

    � Add 'ThresholdCascadeClassifierCV' to 'skfb.ensemble'.

--- skfb/ensemble/__init__.py
+++ skfb/ensemble/__init__.py
@@ -1,10 +1,15 @@
-"""The :mod:`skfb.cascade` module implements cascade ensembles."""
+"""The :mod:`skfb.ensemble` module implements cascade ensembles."""
 
 __all__ = (
     "CascadeNotFittedWarning",
     "SingleModelRouterClassifier",
     "ThresholdCascadeClassifier",
+    "ThresholdCascadeClassifierCV",
 )
 
 from ._router import SingleModelRouterClassifier
-from ._threshold import CascadeNotFittedWarning, ThresholdCascadeClassifier
+from ._threshold import (
+    CascadeNotFittedWarning,
+    ThresholdCascadeClassifier,
+    ThresholdCascadeClassifierCV,
+)
